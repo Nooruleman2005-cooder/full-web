f95ff596aea07a6be831b8f86605c6f981155134
Nooruleman2005-cooder <digitalnooruleman@gmail.com>
2025-08-08 02:28:37 +0500

    full-web

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,14 +1,10 @@
-import axios from 'axios';
-
-const BASE_URL = import.meta.env.VITE_BACKEND_URL;
-
 export const authAxios = axios.create({
-  baseURL: `${BASE_URL}/api/auth`,
+  baseURL: `${import.meta.env.VITE_BACKEND_URL}/api/auth`,
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
 export const productAxios = axios.create({
-  baseURL: `${BASE_URL}/api/products`,
+  baseURL: `${import.meta.env.VITE_BACKEND_URL}/api/products`,
 });
