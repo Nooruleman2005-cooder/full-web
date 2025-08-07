05a64fc0497d351ea85e1a53f17f8aa3ac9a6ca3
Nooruleman2005-cooder <digitalnooruleman@gmail.com>
2025-08-08 02:53:14 +0500

    full-web

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,5 +1,6 @@
 export const authAxios = axios.create({
   baseURL: `${import.meta.env.VITE_BACKEND_URL}/api/auth`,
+    withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
   },
